c8965d094623580ab0c40d1291ae7dfd443cf3fa
ClydeDroid <clydedroid@gmail.com>
2017-01-09 13:21:42 -0500

    Try non secure session cookie

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -48,7 +48,7 @@ Logan::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
