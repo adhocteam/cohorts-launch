b86330676faf87ef5138b096174a031b09dfdc8c
ClydeDroid <clydedroid@gmail.com>
2017-01-25 14:08:59 -0500

    Fix a few pending specs

--- spec/features/tag_creation_and_usage_spec.rb
+++ spec/features/tag_creation_and_usage_spec.rb
@@ -6,7 +6,6 @@ require 'capybara/email/rspec'
 
 feature 'tag person'  do
   scenario 'add tag', js: :true  do
-    pending('something is broken with tokenfield.js')
     person = FactoryGirl.create(:person)
     login_with_admin_user
 
@@ -30,7 +29,6 @@ feature 'tag person'  do
   end
 
   scenario 'delete tag', js: :true  do
-    pending('something is broken with tokenfield.js')
     person = FactoryGirl.create(:person, preferred_contact_method: 'EMAIL')
     login_with_admin_user
 
