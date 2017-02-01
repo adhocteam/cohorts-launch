6874fb732f4278f9593317c6c6891ff99f6e4362
ClydeDroid <clydedroid@gmail.com>
2017-02-01 13:54:04 -0500

    Hide profiler by default

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -1,3 +1,4 @@
 if Rails.env == 'development' && defined? Rack::MiniProfiler
   Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+  Rack::MiniProfiler.config.start_hidden = true 
 end
