fb2b5c9025e5c6b0ca06eba0693a3df10225e90c
ClydeDroid <clydedroid@gmail.com>
2017-02-07 11:24:41 -0500

    Finishing up revamp

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,10 @@
 class DashboardController < ApplicationController
 
   def index
-    @people             = Person.order('created_at DESC').where('signup_at > :startdate AND verified LIKE :verify AND active = :active', { startdate: 1.week.ago, verify: '%Verified%', active: true })
-    @submissions        = Submission.order('created_at DESC').where('person_id is ? AND created_at > ?', nil, 4.weeks.ago)
-    @recent_signups     = @people.size
-    @recent_submissions = @submissions.size
-    @top_five_wards     = Person.group(:geography_id).order('count_all DESC').limit(5).count
-    @bottom_five_wards  = Person.group(:geography_id).order('count_all ASC').limit(5).count
+    @people             = Person.order('created_at DESC').limit(5)
+    @submissions        = Submission.order('created_at DESC').where('person_id is ?', nil).limit(5)
+    @recent_signups     = Person.where('signup_at > :startdate AND active = :active', { startdate: 1.week.ago, active: true }).size
+    @recent_submissions = Submission.where('person_id is ? AND created_at > ?', nil, 4.weeks.ago).size
     @deactivated        = Person.unscoped.where(active: false).where('deactivated_at > ?', 1.week.ago).size
   end
 
