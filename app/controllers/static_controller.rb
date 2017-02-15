8e9ebee3c0dc1d09e98499db0e378adad4abc7c6
Danny Chapman <danny@ducstudio.com>
2017-02-15 13:40:56 -0500

    Design, content for basic general signup form (#3)
    * Design, content for basic general signup form
    
    * Updated form, footer
    
    - Updated form
    - Updated footer
    
    * Add page title to controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,7 +3,11 @@ class StaticController < ApplicationController
   layout 'static'
   skip_before_action :authenticate_user!
 
-  def signup; end
+  def signup
+    @page_title = 'Cohorts'
+  end
 
-  def vets_signup; end
+  def vets_signup
+    @page_title = 'Cohorts: Veteran feedback signup'
+  end
 end
