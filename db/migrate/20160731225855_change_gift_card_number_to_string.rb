a44e3db82945a6d6e980abbc2fb0354e3497a8d9
ClydeDroid <clydedroid@gmail.com>
2017-02-17 11:00:24 -0500

    Make migration reversible

--- db/migrate/20160731225855_change_gift_card_number_to_string.rb
+++ db/migrate/20160731225855_change_gift_card_number_to_string.rb
@@ -1,5 +1,8 @@
 class ChangeGiftCardNumberToString < ActiveRecord::Migration
   def change
-  	change_column :gift_cards, :gift_card_number, :string
+    reversible do |dir|
+  	   dir.up { change_column :gift_cards, :gift_card_number, :string }
+       dir.down { change_column :gift_cards, :gift_card_number, :integer }
+     end
   end
 end
