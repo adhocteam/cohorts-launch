2683ec9c15cf5db93ec4bde76cd84d39de3528d6
ClydeDroid <clydedroid@gmail.com>
2017-03-06 10:40:34 -0500

    Make sure tag factory produces unique tags

--- spec/factories/tag.rb
+++ spec/factories/tag.rb
@@ -2,16 +2,17 @@
 FactoryGirl.define do
   factory :tag, class: Tag do
     name { Faker::Hipster.word }
+    after(:build) do |tag|
+      tag.name += "-#{Faker::Hipster.word}" until tag.valid?
+    end
   end
 
   factory :tagging, class: Tagging do
     taggable_type 'Person'
 
     before(:create) do |tagging|
-      person = FactoryGirl.create(:person)
-      tag    = FactoryGirl.create(:tag)
-      tagging.tag_id = tag.id
-      tagging.taggable_id = person.id
+      tagging.taggable = FactoryGirl.create(:person)
+      tagging.tag = FactoryGirl.create(:tag)
     end
   end
 end
---
+++ spec/features/search_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe 'Search' do
+  describe 'from the menubar' do
+  end
+end
