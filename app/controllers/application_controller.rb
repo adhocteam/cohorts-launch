83953bb583d38c8e3f3e0292d25f5ce2c242a569
Scott Robbin <scott@robbin.co>
2017-03-08 16:51:11 -0600

    Quieted Rubocop about a purportedly unused Devise method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,8 +54,10 @@ class ApplicationController < ActionController::Base
       nil
     end
 
+    # rubocop:disable Lint/UnusedMethodArgument
     def after_sign_out_path_for(resource_or_scope)
       admin_root_path
     end
+  # rubocop:enable Lint/UnusedMethodArgument
 
 end
