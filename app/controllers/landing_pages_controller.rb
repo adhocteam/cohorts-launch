d2fed49acd6043460834dc4d7b2084433c3320fd
Scott Robbin <scott@robbin.co>
2017-03-08 14:19:10 -0600

    Fixed landing page tags bug.
    Unused tags wasn't being loaded on `#create`, so it couldn't display the `#new` view on error.

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class LandingPagesController < ApplicationController
   before_action :find_landing_page, only: [:edit, :update, :destroy]
-  before_action :find_unused_tags, only: [:new, :edit]
+  before_action :find_unused_tags, only: [:new, :create, :edit]
 
   def index
     @landing_pages = LandingPage.joins(:tag).order('tags.name ASC')
