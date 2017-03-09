84fadd08f8655e3f6e1fd5268e248d3ea7ae1151
ClydeDroid <clydedroid@gmail.com>
2017-03-09 13:43:33 -0500

    Show correct number of recent signups on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
   def index
     @people             = Person.order('created_at DESC').limit(5)
     @submissions        = Submission.order('created_at DESC').where('person_id is ?', nil).limit(5)
-    @recent_signups     = Person.where('signup_at > :startdate AND active = :active', { startdate: 1.week.ago, active: true }).size
+    @recent_signups     = Person.where('signup_at > :startdate', { startdate: 1.week.ago }).size
     @recent_submissions = Submission.where('person_id is ? AND created_at > ?', nil, 4.weeks.ago).size
     @deactivated        = Person.unscoped.where(active: false).where('deactivated_at > ?', 1.week.ago).size
   end
